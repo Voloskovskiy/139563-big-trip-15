7c605fbae49e2042573b3547492215e8263794df
Artem Voloskov <artem@voloskov.ru>
2021-09-10 22:04:26 +0300

    Create template parts

---
+++ src/view/create-offer-part-event-edit-form.js
@@ -0,0 +1,10 @@
+export const createSiteOfferPartEventEditFormTemplate = () => (
+  `<div class="event__offer-selector">
+    <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
+    <label class="event__offer-label" for="event-offer-train-1">
+      <span class="event__offer-title">Travel by train</span>
+      &plus;&euro;&nbsp;
+      <span class="event__offer-price">40</span>
+    </label>
+  </div>`
+);
